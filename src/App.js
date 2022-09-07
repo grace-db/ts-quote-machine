da442e118b0e16e7659f5d49ae4b2a279d9dde02
Grace <92275742+grace-mccarty@users.noreply.github.com>
2022-09-07 14:10:59 -0500

    first official commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+
 
 function App() {
+  const [lyric, setLyric] = useState('');
+  const [artist, setArtist] = useState('');
+  useEffect(() => {
+    getLyrics();
+  }, []);
+
+const getLyrics = () => {
+  fetch("https://gist.githubusercontent.com/grace-mccarty/c77da583eb442ed35c890332f9efdfe8/raw/137243dddaced1faef88100b906a698c0779b228/tswift-lyrics.json")
+    .then((res) => res.json())
+    .then((song) => {
+      let songLy = song.lyrics;
+      let randNum = Math.floor(Math.random() * songLy.length);
+      let randLy = songLy[randNum];
+      setLyric(randLy.lyric);
+      setArtist(randLy.artist);
+    });
+};
+
+const nextLyric = () => {
+  getLyrics();
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main id="container">
+      <div id="quote-box">
+        <p id="text">{lyric}</p>
+        <div id="btm-row">
+          <p id="author">- {artist}</p>
+          <button id="new-quote" onClick={nextLyric}>swiftify~</button>
+          <a className="twitter-share-button" id="tweet-quote" href="https://twitter.com/intent/tweet" target="_blank" rel="noreferrer">
+            <i className="fa-brands fa-twitter"></i>
+          </a>
+        </div>
+        <p id="final-p">All lyrics belong to Taylor Swift</p>
+      </div>
+    </main>
   );
 }
 
